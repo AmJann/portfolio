8475c69745d395cf8e50b67d18fb1202e489323b
Amber Jankowski <amberbjankowski@gmail.com>
2024-07-18 09:55:17 -0400

    updated resume and skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
-import '../css/skills.css'
+import React from "react";
+import "../css/skills.css";
 
 function Skills() {
   return (
     <div>
-        <div>
-            <h3 className='abril-fatface-regular skillsTitle'>Skills</h3>
-        </div>
-        <div className='skillsContainer'>
-    <h3 className='lato-regular'><span className='abril-fatface-regular'>Languages</span> - JavaScript, HTML, CSS, Python, SQL, Mongo, JSON, EJS</h3>
-<h3 className='lato-regular'><span className='abril-fatface-regular'>Database</span> - PostgreSQL, MongoDB | <span className='abril-fatface-regular'>Other</span> - RESTful Routing, JSON api, Node.js</h3>
-<h3 className='lato-regular'><span className='abril-fatface-regular'>Libraries and Frameworks</span> - React, Redux, Vite, Express.js, Django, Flask</h3>
-        </div>
+      <div>
+        <h3 className="abril-fatface-regular skillsTitle">Skills</h3>
+      </div>
+      <div className="skillsContainer">
+        <h3 className="lato-regular">
+          <span className="abril-fatface-regular">Languages</span> - JavaScript,
+          HTML, CSS, Python, SQL, Mongo, JSON, EJS
+        </h3>
+        <h3 className="lato-regular">
+          <span className="abril-fatface-regular">Database</span> - PostgreSQL,
+          MongoDB | <span className="abril-fatface-regular">Other</span> -
+          RESTful Routing, JSON api
+        </h3>
+        <h3 className="lato-regular">
+          <span className="abril-fatface-regular">
+            Libraries and Frameworks
+          </span>{" "}
+          - React, Redux, Vite, Express.js, Django, Flask, Phaser
+        </h3>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Skills
+export default Skills;
--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,31 +1,44 @@
-import React from 'react'
+import React from "react";
 
-import resume from '../images/Amber-J-March24.pdf'
-import'../css/socialLinks.css'
-import { Link } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
-import { faFile } from '@fortawesome/free-solid-svg-icons';
-
+import resume from "../images/Amber-J-July24p.pdf";
+import "../css/socialLinks.css";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faFile } from "@fortawesome/free-solid-svg-icons";
 
 function Social() {
   return (
-    <div className='header'>
-    <div className='socialLinks'>
-        <Link target="_blank" rel="noreferrer" to='https://github.com/AmJann'><FontAwesomeIcon className='socialButton' icon={faGithub} /></Link>
-        <Link target="_blank" rel="noreferrer" to='https://www.linkedin.com/in/amber-jankowski/'><FontAwesomeIcon className='socialButton' icon={faLinkedin} /></Link>
-        <Link target="_blank" rel="noreferrer" to={resume}><FontAwesomeIcon className='socialButton' icon={faFile} /></Link>
-    </div>
-    <div className='title'>
-    <div>
-            <h1 className='abril-fatface-regular fontSizeTitleName'>Amber Jankowski</h1>
-    </div>
+    <div className="header">
+      <div className="socialLinks">
+        <Link target="_blank" rel="noreferrer" to="https://github.com/AmJann">
+          <FontAwesomeIcon className="socialButton" icon={faGithub} />
+        </Link>
+        <Link
+          target="_blank"
+          rel="noreferrer"
+          to="https://www.linkedin.com/in/amber-jankowski/"
+        >
+          <FontAwesomeIcon className="socialButton" icon={faLinkedin} />
+        </Link>
+        <Link target="_blank" rel="noreferrer" to={resume}>
+          <FontAwesomeIcon className="socialButton" icon={faFile} />
+        </Link>
+      </div>
+      <div className="title">
         <div>
-            <h3 className='montserrat-alternates-medium fontSizeTitle'>Software engineer</h3>
+          <h1 className="abril-fatface-regular fontSizeTitleName">
+            Amber Jankowski
+          </h1>
         </div>
+        <div>
+          <h3 className="montserrat-alternates-medium fontSizeTitle">
+            Software engineer
+          </h3>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Social
+export default Social;
